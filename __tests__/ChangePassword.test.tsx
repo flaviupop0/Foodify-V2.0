d5bf216a0762165ceabbd248d0e59a9b83e6ba0c
flaviupop0 <flaviupop2001@gmail.com>
2025-04-03 15:52:49 +0300

    Test to render success change password

--- __tests__/ChangePassword.test.tsx
+++ __tests__/ChangePassword.test.tsx
@@ -5,6 +5,7 @@
 import 'react-native';
 import React from 'react';
 import ChangePassword from '../app/screens/ChangePassword/ChangePassword';
+import SuccessChangePassword from '../app/screens/ChangePassword/SuccessChangePassword';
 import {render} from '@testing-library/react-native';
 import {it, jest} from '@jest/globals';
 
@@ -15,4 +16,8 @@ const mockNavigation = {
 
 it('renders correctly', () => {
   render(<ChangePassword navigation={mockNavigation} />);
+});
+
+it('renders correctly the success change password', () => {
+  render(<SuccessChangePassword navigation={mockNavigation} />);
 });
