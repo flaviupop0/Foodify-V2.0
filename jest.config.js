6a3d168974f1474bd9fa7f48f83a8c577dc26498
flaviupop0 <flaviupop2001@gmail.com>
2025-04-03 12:08:52 +0300

    Added test coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      lines: 80,
+    },
+  },
 };
